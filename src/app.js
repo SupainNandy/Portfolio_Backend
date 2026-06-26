c2cfb761ff6357ea57b8ecfd968fde916a430ac2
Supain <supainnandy1234@gmail.com>
2026-06-26 15:51:07 +0530

    cors app update

--- src/app.js
+++ src/app.js
@@ -1,23 +1,47 @@
-const express = require("express");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
+    const express = require('express')
+    const cors = require('cors')
+    const cookieParser = require('cookie-parser')
 
-const app = express();
+    const app = express()
+   const cors = require("cors");
 
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://supain-portfolio.vercel.app"
-    ],
-    credentials: true
+  origin: [
+    "http://localhost:5173",
+    "https://supain-portfolio.vercel.app"
+  ],
+  credentials: true
 }));
+    app.use(express.json())
+    app.use(express.urlencoded({extended:true}))
+    app.use(cookieParser())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+    app.get("/",(req,res)=>{
+        res.send("Hello from server")
+    })
+    //Adin Auth
+    const adminRoutes = require('../src/Routes/admin.routes')
+    app.use('/admin/auth',adminRoutes)
 
-// Routes
-// app.use("/api/projects", projectRouter);
-// app.use("/api/admin", adminRouter);
+    //Admin Profile\
+    const profileRoutes = require('../src/Routes/profile.routes')
+    app.use('/admin/profile',profileRoutes)
 
-module.exports = app;
+    //Education Admin
+    const educationRoutes = require("../src/Routes/education.routes")
+    app.use('/admin/education',educationRoutes)
+
+    //Skill Admin
+    const skillRoutes = require('../src/Routes/skill.routes')
+    app.use('/admin/skill',skillRoutes)
+
+    //Project Admin 
+    const projectRoutes = require('../src/Routes/project.routes')
+
+    app.use('/admin/project',projectRoutes)
+
+    //Resume Admin
+    const resumeRoutes = require('../src/Routes/resume.routes')
+    app.use('/admin/resume',resumeRoutes)
+
+    module.exports=app
