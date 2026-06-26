a2a6c6a9342f5474181d636862b767b144fdb50a
Supain <supainnandy1234@gmail.com>
2026-06-26 13:54:27 +0530

    Cors Update

--- src/app.js
+++ src/app.js
@@ -3,10 +3,15 @@
     const cookieParser = require('cookie-parser')
 
     const app = express()
-    app.use(cors({
-        origin:"http://localhost:5173",
-        credentials:true
-    }))
+   const cors = require("cors");
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://supain-portfolio.vercel.app"
+  ],
+  credentials: true
+}));
     app.use(express.json())
     app.use(express.urlencoded({extended:true}))
     app.use(cookieParser())
